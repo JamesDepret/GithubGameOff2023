6e7c7969052f6ae90117d520e6b50b79e3ecc5da
JamesDepret <jamesdepret@gmail.com>
2023-11-12 22:50:39 +0100

    major bugfix => upgrades not working

--- scenes/ability/BaseController.cs
+++ scenes/ability/BaseController.cs
@@ -2,6 +2,7 @@ namespace Ability;
 public  abstract partial class BaseAbilityController : Node
 {
     public string SubName { get; set; } = "";
+    public BaseUpgrade Upgrade { get; set; }
     public virtual void Init() {}
     public virtual void DoEffect() {}
 }
--- scenes/character/player/PlayerAbilities.cs
+++ scenes/character/player/PlayerAbilities.cs
@@ -2,14 +2,27 @@ namespace Character;
 
 public partial class Player : CharacterBody2D
 {    
+    List<BaseAbilityController> shipTurrets_Abilities = new();
     void OnNewShipTurretAdded(BaseUpgrade upgrade, Godot.Collections.Array<BaseUpgrade> currentUpgrades)
     {
         if (upgrade.AbilityControllerScene == null) return;
-        upgrade.PreviousUpgradePointer?.ControllerPointer?.QueueFree();
+        if(upgrade.PreviousUpgradePointer != null)
+        {
+            // all the upgrades point to the same resource so when they get get upgraded, the next upgrade would throw
+            // a disposed error because we deleted it's pointer. So we just find one controller to delete instead
+            var upg = shipTurrets_Abilities.Find(x => x.SubName == upgrade.PreviousUpgradePointer?.Id);
+            if (upg != null)
+            {
+                upg.QueueFree();
+                shipTurrets_Abilities.Remove(upg);
+            }
+        }
         var controller = upgrade.AbilityControllerScene.Instantiate() as BaseAbilityController;
         controller.SubName = upgrade.Id;
+        controller.Upgrade = upgrade;
         shipTurrets_AbilitiesNode.AddChild(controller);
         controller.Init();
         upgrade.ControllerPointer = controller;
+        shipTurrets_Abilities.Add(controller);
     }
 }
